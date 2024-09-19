7237904d40f3e09c0d5f07691318d76bd2637987
Rowan Jones <rowan@impermanent.io>
2024-09-19 19:58:57 +1000

    tidy up

--- src/quantized_mesh.rs
+++ src/quantized_mesh.rs
@@ -2,6 +2,8 @@ use binrw::helpers::until_eof;
 use binrw::{binread, BinRead, BinResult};
 use nalgebra::{Matrix4, Point3, Vector3};
 
+// Geospatial structs & functions
+
 /// Ellipsoid constants for WGS-84
 #[derive(Debug)]
 pub struct Ellipsoid {
@@ -42,16 +44,56 @@ impl Ellipsoid {
 
 #[derive(Debug)]
 pub struct BoundingBox {
-    lower_left: (f64, f64),  
+    lower_left: (f64, f64),
     upper_right: (f64, f64),
 }
 
 impl BoundingBox {
     pub fn dimensions(&self) -> (f64, f64) {
-        let width = self.upper_right.0 - self.lower_left.0; 
-        let height = self.upper_right.1 - self.lower_left.1; 
+        let width = self.upper_right.0 - self.lower_left.0;
+        let height = self.upper_right.1 - self.lower_left.1;
         (width, height)
     }
+}
+
+#[derive(Debug)]
+pub struct GeodeticPoint3 {
+    lat: f64,
+    lon: f64,
+    alt: f64,
+}
+
+pub trait ToDegrees {
+    fn to_degrees(self) -> GeodeticPoint3;
+}
+
+impl ToDegrees for GeodeticPoint3 {
+    fn to_degrees(self) -> GeodeticPoint3 {
+        GeodeticPoint3 {
+            lat: self.lat.to_degrees(),
+            lon: self.lon.to_degrees(),
+            alt: self.alt,
+        }
+    }
+}
+
+pub trait ToRadians {
+    fn to_radians(self) -> f64;
+}
+
+impl ToRadians for f64 {
+    fn to_radians(self) -> f64 {
+        self * std::f64::consts::PI / 180.0
+    }
+}
+
+#[binread]
+#[derive(Debug)]
+#[br(little)]
+pub struct BoundingSphere {
+    #[br(parse_with = parse_point3)]
+    pub center: Point3<f64>,
+    pub radius: f64,
 }
 
 /// Convert Cartesian ECEF coordinates to geodetic coordinates
@@ -122,54 +164,173 @@ pub fn calculate_enu_to_ecef_rotation_matrix(
     )
 }
 
+// Linear interpolation function
+fn lerp(min_value: f64, max_value: f64, t: f64) -> f64 {
+    min_value + t * (max_value - min_value)
+}
+
+/// Convert WorldCRS84Quad tile to bounding box (lat/lon).
+/// Returns a LL/UR bounding box
+pub fn tile_to_bbox_crs84(x: u32, y: u32, z: u32) -> (BoundingBox) {
+    let tiles_per_side = 2 << z; // 2 tiles at 0 level for WGS84
+    let tile_size_deg = 360.0 / tiles_per_side as f64; // Tile size in degrees
+
+    // Longitude bounds
+    let min_lon = x as f64 * tile_size_deg - 180.0;
+    let max_lon = (x as f64 + 1.0) * tile_size_deg - 180.0;
+
+    // Latitude boundss
+    let min_lat = (y as f64) * tile_size_deg - 90.0;
+    let max_lat = (y as f64 + 1.0) * tile_size_deg - 90.0;
+
+    BoundingBox {
+        lower_left: (min_lon, min_lat),
+        upper_right: (max_lon, max_lat),
+    }
+}
+
+// Quantized Mesh Binary Handling
+
 #[binread]
 #[derive(Debug)]
-#[repr(u8)]
-#[br(repr = u8)]
-pub enum ExtensionID {
-    TerrainLighting = 1,
-    WaterMask = 2,
-    Metadata = 4,
-}
+#[br(little)]
+pub struct QuantizedMeshHeader {
+    #[br(dbg)]
+    #[br(parse_with = parse_point3)]
+    pub center: Point3<f64>,
 
-#[derive(Debug)]
-pub struct GeodeticPoint3 {
-    lat: f64, 
-    lon: f64,
-    alt: f64, 
-}
+    #[br(dbg)]
+    pub min_height: f32,
 
-pub trait ToDegrees {
-    fn to_degrees(self) -> GeodeticPoint3;
-}
+    #[br(dbg)]
+    pub max_height: f32,
 
-impl ToDegrees for GeodeticPoint3 {
-    fn to_degrees(self) -> GeodeticPoint3 {
-        GeodeticPoint3 {
-            lat: self.lat.to_degrees(),
-            lon: self.lon.to_degrees(),
-            alt: self.alt, 
-        }
-    }
-}
+    #[br(dbg)]
+    pub bounding_sphere: BoundingSphere,
 
-pub trait ToRadians {
-    fn to_radians(self) -> f64;
-}
-
-impl ToRadians for f64 {
-    fn to_radians(self) -> f64 {
-        self * std::f64::consts::PI / 180.0
-    }
+    #[br(dbg)]
+    #[br(parse_with = parse_point3)]
+    pub horizon_occlusion_point: Point3<f64>,
 }
 
 #[binread]
 #[derive(Debug)]
 #[br(little)]
-pub struct BoundingSphere {
-    #[br(parse_with = parse_point3)]
-    pub center: Point3<f64>,
-    pub radius: f64,
+pub struct VertexData {
+    #[br(dbg)]
+    pub vertex_count: u32,
+
+    #[br(count = vertex_count)]
+    #[br(map = |i:  Vec<u16>| i.vec_zigzag_decode() )]
+    pub u: Vec<i32>,
+
+    #[br(count = vertex_count)]
+    #[br(map = |i:  Vec<u16>| i.vec_zigzag_decode())]
+    pub v: Vec<i32>,
+
+    #[br(count = vertex_count)]
+    #[br(map = |i:  Vec<u16>| i.vec_zigzag_decode())]
+    pub height: Vec<i32>,
+
+    #[br(dbg)]
+    pub triangleCount: u32,
+
+    #[br(align_before = 4)]
+    #[br(count = triangleCount * 3)]
+    #[br(if(vertex_count > 65536) )]
+    #[br(map = |i:  Vec<u32>| Some(i.high_watermark_decode()))]
+    pub index_data_long: Option<Vec<usize>>,
+
+    #[br(align_before = 2)]
+    #[br(count = triangleCount * 3)]
+    #[br(if(vertex_count <= 65536))]
+    #[br(map = |i: Vec<u16>| {
+        let u32_vec: Vec<u32> = i.into_iter().map(|x| x as u32).collect();
+        Some(u32_vec.high_watermark_decode())
+    })]
+    pub index_data_short: Option<Vec<usize>>,
+
+    #[br(align_before = 4)]
+    #[br(if(vertex_count > 65536) )]
+    pub edge_indices_long: Option<EdgeIndicesLong>,
+
+    #[br(align_before = 2)]
+    #[br(if(vertex_count <= 65536) )]
+    pub edge_indices_short: Option<EdgeIndicesShort>,
+}
+
+#[binread]
+#[derive(Debug)]
+#[br(little)]
+
+pub struct QuantizedMesh {
+    pub header: QuantizedMeshHeader,
+    pub vertex_data: VertexData,
+
+    #[br(parse_with = until_eof)]
+    pub extensions: Vec<Extension>,
+}
+
+impl QuantizedMesh {
+    fn get_interpolated_vertex(&self, vertex_index: usize, bbox: &BoundingBox) -> Point3<f64> {
+        let u_value = self.vertex_data.u[vertex_index] as f64;
+        let v_value = self.vertex_data.v[vertex_index] as f64;
+        let height_value = self.vertex_data.height[vertex_index] as f64;
+
+        let (min_lon, min_lat) = bbox.lower_left; // Lower-left corner
+        let (max_lon, max_lat) = bbox.upper_right; // Upper-right corner
+
+        // Lerp for longitude (x) and latitude (y)
+        let x = lerp(min_lon, max_lon, u_value / 32767.0);
+        let y = lerp(min_lat, max_lat, v_value / 32767.0);
+
+        // Lerp for height (z)
+        let z = lerp(
+            self.header.min_height as f64,
+            self.header.max_height as f64,
+            height_value / 32767.0,
+        );
+
+        Point3::new(x, y, z)
+    }
+    pub fn get_triangle(
+        &self,
+        triangle_index: usize,
+        bbox: &BoundingBox,
+    ) -> Option<[Point3<f64>; 3]> {
+        // long or short index?
+        let index_data = match (
+            self.vertex_data.index_data_long.as_ref(),
+            self.vertex_data.index_data_short.as_ref(),
+        ) {
+            (Some(long), _) => long,
+            (None, Some(short)) => short,
+            (None, None) => return None,
+        };
+
+        let triangle_vertices = [
+            index_data[triangle_index * 3],
+            index_data[triangle_index * 3 + 1],
+            index_data[triangle_index * 3 + 2],
+        ];
+
+        // Check vertex index bounds
+        if triangle_vertices
+            .iter()
+            .any(|&x| x >= self.vertex_data.vertex_count as usize)
+        {
+            return None; // Out of bounds
+        }
+
+        // Calculate interpolated coordinates for each vertex
+        let triangle: [nalgebra::OPoint<f64, nalgebra::Const<3>>; 3] = [
+            self.get_interpolated_vertex(triangle_vertices[0], &bbox),
+            self.get_interpolated_vertex(triangle_vertices[1], &bbox),
+            self.get_interpolated_vertex(triangle_vertices[2], &bbox),
+        ];
+
+        Some(triangle)
+    }
 }
 
 pub trait EdgeIndices {}
@@ -237,48 +398,26 @@ pub struct EdgeIndicesLong {
 
 #[binread]
 #[derive(Debug)]
+#[repr(u8)]
+#[br(repr = u8)]
+pub enum ExtensionID {
+    TerrainLighting = 1,
+    WaterMask = 2,
+    Metadata = 4,
+}
+
+#[binread]
+#[derive(Debug)]
 #[br(little)]
-pub struct VertexData {
+
+pub struct Extension {
     #[br(dbg)]
-    pub vertex_count: u32,
-
-    #[br(count = vertex_count)]
-    #[br(map = |i:  Vec<u16>| vec_zigzag_decode(i) )]
-    pub u: Vec<i32>,
-
-    #[br(count = vertex_count)]
-    #[br(map = |i:  Vec<u16>| vec_zigzag_decode(i))]
-    pub v: Vec<i32>,
-
-    #[br(count = vertex_count)]
-    #[br(map = |i:  Vec<u16>| vec_zigzag_decode(i))]
-    pub height: Vec<i32>,
-
+    pub extensionId: u8,
     #[br(dbg)]
-    pub triangleCount: u32,
+    pub extensionLength: u32, // bytes
 
-    #[br(align_before = 4)]
-    #[br(count = triangleCount * 3)]
-    #[br(if(vertex_count > 65536) )]
-    #[br(map = |i:  Vec<u32>| Some(i.high_watermark_decode()))]
-    pub index_data_long: Option<Vec<usize>>,
-
-    #[br(align_before = 2)]
-    #[br(count = triangleCount * 3)]
-    #[br(if(vertex_count <= 65536))]
-    #[br(map = |i: Vec<u16>| {
-        let u32_vec: Vec<u32> = i.into_iter().map(|x| x as u32).collect();
-        Some(u32_vec.high_watermark_decode())
-    })]
-    pub index_data_short: Option<Vec<usize>>,
-
-    #[br(align_before = 4)]
-    #[br(if(vertex_count > 65536) )]
-    pub edge_indices_long: Option<EdgeIndicesLong>,
-
-    #[br(align_before = 2)]
-    #[br(if(vertex_count <= 65536) )]
-    pub edge_indices_short: Option<EdgeIndicesShort>,
+    #[br(count = extensionLength)]
+    pub extensionData: Vec<u8>,
 }
 
 trait HighWatermarkDecode {
@@ -301,125 +440,21 @@ impl HighWatermarkDecode for Vec<u32> {
     }
 }
 
-fn vec_zigzag_decode(src: Vec<u16>) -> Vec<i32> {
-    let mut res: Vec<i32> = Vec::with_capacity(src.len());
-    let mut n: i32 = 0;
-    for i in src.iter() {
-        let j: i32 = *i as i32;
-        // zigzag decode
-        n += (j >> 1) ^ (-(j & 1));
-        res.push(n);
-    }
-    res
+trait ZigZagDecode {
+    fn vec_zigzag_decode(&self) -> Vec<i32>;
 }
 
-#[binread]
-#[derive(Debug)]
-#[br(little)]
-pub struct QuantizedMeshHeader {
-    #[br(dbg)]
-    #[br(parse_with = parse_point3)]
-    pub center: Point3<f64>,
-
-    #[br(dbg)]
-    pub min_height: f32,
-
-    #[br(dbg)]
-    pub max_height: f32,
-
-    #[br(dbg)]
-    pub bounding_sphere: BoundingSphere,
-
-    #[br(dbg)]
-    #[br(parse_with = parse_point3)]
-    pub horizon_occlusion_point: Point3<f64>,
-}
-
-#[binread]
-#[derive(Debug)]
-#[br(little)]
-
-pub struct Extension {
-    #[br(dbg)]
-    pub extensionId: u8,
-    #[br(dbg)]
-    pub extensionLength: u32, // bytes
-
-    #[br(count = extensionLength)]
-    pub extensionData: Vec<u8>,
-}
-
-#[binread]
-#[derive(Debug)]
-#[br(little)]
-
-pub struct QuantizedMesh {
-    pub header: QuantizedMeshHeader,
-    pub vertex_data: VertexData,
-
-    #[br(parse_with = until_eof)]
-    pub extensions: Vec<Extension>,
-}
-
-impl QuantizedMesh {
-    fn get_interpolated_vertex(&self, vertex_index: usize, bbox: &BoundingBox) -> Point3<f64> {
-        let u_value = self.vertex_data.u[vertex_index] as f64;
-        let v_value = self.vertex_data.v[vertex_index] as f64;
-        let height_value = self.vertex_data.height[vertex_index] as f64;
-
-        let (min_lon, min_lat) = bbox.lower_left; // Lower-left corner
-        let (max_lon, max_lat) = bbox.upper_right; // Upper-right corner
-
-        // Lerp for longitude (x) and latitude (y)
-        let x = lerp(min_lon, max_lon, u_value / 32767.0);
-        let y = lerp(min_lat, max_lat, v_value / 32767.0);
-
-        // Lerp for height (z)
-        let z = lerp(
-            self.header.min_height as f64,
-            self.header.max_height as f64,
-            height_value / 32767.0,
-        );
-
-        Point3::new(x, y, z)
-    }
-    pub fn get_triangle(
-        &self,
-        triangle_index: usize,
-        bbox: &BoundingBox,
-    ) -> Option<[Point3<f64>; 3]> {
-        // long or short index?
-        let index_data = match (
-            self.vertex_data.index_data_long.as_ref(),
-            self.vertex_data.index_data_short.as_ref(),
-        ) {
-            (Some(long), _) => long,
-            (None, Some(short)) => short,
-            (None, None) => return None, 
-        };
-
-        let triangle_vertices = [
-            index_data[triangle_index * 3],
-            index_data[triangle_index * 3 + 1],
-            index_data[triangle_index * 3 + 2],
-        ];
-
-        // Check vertex index bounds
-        if triangle_vertices
-            .iter()
-            .any(|&x| x >= self.vertex_data.vertex_count as usize)
-        {
-            return None; // Out of bounds
+impl ZigZagDecode for Vec<u16> {
+    fn vec_zigzag_decode(&self) -> Vec<i32> {
+        let mut res: Vec<i32> = Vec::with_capacity(self.len());
+        let mut n: i32 = 0;
+        for i in self.iter() {
+            let j: i32 = *i as i32;
+            // zigzag decode
+            n += (j >> 1) ^ (-(j & 1));
+            res.push(n);
         }
-
-        // Calculate interpolated coordinates for each vertex
-        let triangle: [nalgebra::OPoint<f64, nalgebra::Const<3>>; 3] = [
-            self.get_interpolated_vertex(triangle_vertices[0], &bbox),
-            self.get_interpolated_vertex(triangle_vertices[1], &bbox),
-            self.get_interpolated_vertex(triangle_vertices[2], &bbox),
-        ];
-
-        Some(triangle)
+        res
     }
 }
 
@@ -430,29 +465,4 @@ fn parse_point3() -> BinResult<Point3<f64>> {
         <_>::read_options(reader, endian, ())?,
         <_>::read_options(reader, endian, ())?,
     ))
-}
-
-// Linear interpolation function
-fn lerp(min_value: f64, max_value: f64, t: f64) -> f64 {
-    min_value + t * (max_value - min_value)
-}
-
-/// Convert WorldCRS84Quad tile to bounding box (lat/lon).
-/// Returns a LL/UR bounding box
-pub fn tile_to_bbox_crs84(x: u32, y: u32, z: u32) -> (BoundingBox) {
-    let tiles_per_side = 2 << z; // 2 tiles at 0 level for WGS84
-    let tile_size_deg = 360.0 / tiles_per_side as f64; // Tile size in degrees
-
-    // Longitude bounds
-    let min_lon = x as f64 * tile_size_deg - 180.0;
-    let max_lon = (x as f64 + 1.0) * tile_size_deg - 180.0;
-
-    // Latitude boundss
-    let min_lat = (y as f64) * tile_size_deg - 90.0;
-    let max_lat = (y as f64 + 1.0) * tile_size_deg - 90.0;
-
-    BoundingBox {
-        lower_left: (min_lon, min_lat),
-        upper_right: (max_lon, max_lat),
-    }
 }
