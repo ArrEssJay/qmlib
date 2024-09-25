e56f528c8d194121d12baaf9af8b5b0c56736f35
Rowan Jones <rowan@impermanent.io>
2024-09-25 18:43:19 +1000

    update tile info

--- app/src/tile_info.rs
+++ app/src/tile_info.rs
@@ -21,50 +21,70 @@ fn main() -> Result<(), String> { // Update Result to specify the error type
         Ok(tile) => {
             // Get bounding box
             println!("Tile Bounding Rectangle: {:#?}", tile.quantized_mesh.bounding_box);
-            println!(
-                "Tile Bounding Rectangle Dimensions: {:#?}",
-                tile.quantized_mesh.bounding_box.dimensions()
-            );
 
+             // Print center of tile in ecef and lat/lon
+             println!("Tile Centre (Geocentric): {:#?}", tile.quantized_mesh.header.center);
+
+             let centre_geodetic: GeodeticPoint3 =
+                 qmlib::ecef_to_geodetic(&tile.quantized_mesh.header.center, &tile.quantized_mesh.ellipsoid);
+             println!(
+                 "Tile Centre (Geodetic): {:#?}",
+                 centre_geodetic.to_degrees()
+             );
+ 
+             // Calculate the ENU to ECEF rotation matrix and the distance to the bounding sphere center
+             let to_enu_matrix =
+                 qmlib::calculate_enu_to_ecef_rotation_matrix(tile.quantized_mesh.header.center, &tile.quantized_mesh.ellipsoid)
+                     .transpose();
+             let dist_enu = to_enu_matrix
+                 .transform_vector(&(tile.quantized_mesh.header.bounding_sphere.center - tile.quantized_mesh.header.center));
+             println!(
+                 "Tile Centre -> Bounding Sphere Centre (ENU from tile centre): {:#?}",
+                 dist_enu
+             );
+
+            println!("\n\nVertex Count (U): {:#?}", tile.quantized_mesh.vertex_data.u.len());
+            println!("Vertex Count (V): {:#?}", tile.quantized_mesh.vertex_data.v.len());
+            println!("Vertex Count (Height): {:#?}", tile.quantized_mesh.vertex_data.height.len());
+
+            println!("\n\nTriangle Count: {:#?}", tile.quantized_mesh.vertex_data.triangle_count);
+            println!("Triangle Long Index: {:#?}", tile.quantized_mesh.vertex_data.index_data_long.is_some());
+            println!("Triangle Short Index: {:#?}", tile.quantized_mesh.vertex_data.index_data_short.is_some());
+            
+
+            println!("\nLong Edge Index: {:#?}", tile.quantized_mesh.vertex_data.edge_indices_long.is_some());
+            println!("Short Edge Index: {:#?}", tile.quantized_mesh.vertex_data.edge_indices_short.is_some());
+
+            if let Some(edge_indices_long) = &tile.quantized_mesh.vertex_data.edge_indices_long {
+                // Now you can use edge_indices_long safely
+                // For example:
+                println!("East Edge Vertex Count: {:?}", edge_indices_long.east_vertex_count);
+                println!("West Edge Vertex Count: {:?}", edge_indices_long.west_vertex_count);
+                println!("North Edge Vertex Count: {:?}", edge_indices_long.north_vertex_count);
+                println!("South Edge Vertex Count: {:?}", edge_indices_long.south_vertex_count);
+
+            }
+
+            if let Some(edge_indices_short) = &tile.quantized_mesh.vertex_data.edge_indices_short {
+                // Now you can use edge_indices_long safely
+                // For example:
+                println!("East Edge Vertex Count: {:?}", edge_indices_short.east_vertex_count);
+                println!("West Edge Vertex Count: {:?}", edge_indices_short.west_vertex_count);
+                println!("North Edge Vertex Count: {:?}", edge_indices_short.north_vertex_count);
+                println!("South Edge Vertex Count: {:?}", edge_indices_short.south_vertex_count);
+
+            } 
+          
             // Process extensions - TODO
             for ext in &tile.quantized_mesh.extensions {
-                println!(" - {:?}", ext.extension_id);
                 if ext.extension_id == 4 {
                     let s = str::from_utf8(&ext.extension_data)
                         .unwrap_or_else(|e| panic!("Invalid UTF-8 sequence: {}", e));
-                    println!("Metadata extension: \n{:?}", s);
+                    println!("\nMetadata Extension (4): \n{:?}", s);
                 }
             }
-            // Print center of tile in ecef and lat/lon
-            println!("Tile Centre (Geocentric): {:#?}", tile.quantized_mesh.header.center);
+           
 
-            let centre_geodetic: GeodeticPoint3 =
-                qmlib::ecef_to_geodetic(&tile.quantized_mesh.header.center, &tile.quantized_mesh.ellipsoid);
-            println!(
-                "Tile Centre (Geodetic): {:#?}",
-                centre_geodetic.to_degrees()
-            );
-
-            // Calculate the ENU to ECEF rotation matrix and the distance to the bounding sphere center
-            let to_enu_matrix =
-                qmlib::calculate_enu_to_ecef_rotation_matrix(tile.quantized_mesh.header.center, &tile.quantized_mesh.ellipsoid)
-                    .transpose();
-            let dist_enu = to_enu_matrix
-                .transform_vector(&(tile.quantized_mesh.header.bounding_sphere.center - tile.quantized_mesh.header.center));
-            println!(
-                "Tile Centre -> Bounding Sphere Centre (ENU from tile centre): {:#?}",
-                dist_enu
-            );
-
-            // Print the first triangle
-            println!("0: {:#?}", tile.quantized_mesh.get_triangle(0, true));
-
-            // Print the first triangle
-            println!("Vertices: {:#?}", tile.quantized_mesh.geodetic_vertices);
-
-            println!("Vertices u: {:#?}", tile.quantized_mesh.vertex_data.u);
-            println!("Vertices v: {:#?}", tile.quantized_mesh.vertex_data.v);
-            println!("Vertices h: {:#?}", tile.quantized_mesh.vertex_data.height);
         }
         Err(err) => {
             eprintln!("Error: {}", err);
