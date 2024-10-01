b2196c4e522612e24f7a740749c925bf8a607364
Rowan Jones <rowan@impermanent.io>
2024-10-02 02:17:37 +1000

    georeference tiff

--- lib/src/tiff_writer.rs
+++ lib/src/tiff_writer.rs
@@ -1,8 +1,13 @@
 use nalgebra::Point2;
 use std::{error::Error, fs::File, path::Path};
-use tiff::encoder::{colortype::Gray32Float, TiffEncoder};
+use tiff::{
+    encoder::{colortype::Gray32Float, TiffEncoder},
+    tags::Tag,
+};
 
-use crate::{geometry::Triangle, raster::interpolate_height_in_triangle, QuantizedMesh, UV_MAX_U16};
+use crate::{
+    geometry::Triangle, raster::interpolate_height_in_triangle, QuantizedMesh, UV_MAX_U16,
+};
 
 pub fn get_raster_size(scale_shift: u16) -> u16 {
     (UV_MAX_U16 + 1) >> scale_shift
@@ -32,7 +37,7 @@ pub fn rasterize(qm: &QuantizedMesh, scale_shift: u16) -> Vec<Option<f32>> {
                 ),
             ],
         };
-        
+
         let tri_heights = [
             heights[triangle_indices[0]],
             heights[triangle_indices[1]],
@@ -63,8 +68,6 @@ pub fn rasterize(qm: &QuantizedMesh, scale_shift: u16) -> Vec<Option<f32>> {
     raster
 }
 
-
-
 pub fn export_to_tiff(
     qm: &QuantizedMesh,
     filename: &Path,
@@ -73,7 +76,74 @@ pub fn export_to_tiff(
     let raster_size = get_raster_size(scale_shift);
     let raster = rasterize(qm, scale_shift);
 
-    let mut encoder: TiffEncoder<File> = TiffEncoder::new(File::create(filename)?)?;
+    let mut tiff: TiffEncoder<File> = TiffEncoder::new(File::create(filename)?)?;
+    let mut image = tiff
+        .new_image::<Gray32Float>(raster_size.into(), raster_size.into())
+        .unwrap();
+
+    let geo_key_directory = vec![
+    1, 1, 0, 2,          // Header: Version 1.1, 8 keys
+    1024, 0, 1, 2,      // GTRasterTypeGeoKey 2 - geographic
+    2048, 0, 1, 4979,   // GeodeticCRSGeoKey, value 4979 (WGS 84 3D)
+    4096, 0, 1, 4979,   // VerticalGeoKey, value 4979 (WGS 84 3D)
+    //0x1003, 0, 1, 9001,  // VerticalUnit, value 9001 (Meter)
+   // 0x1005, 0, 1, 9102,  // HorizontalUnit, value 9102 (Degree)
+   // 0x1004, 0, 1, 9102,  // GeographicUnit, value 9102 (Degree)
+   // 0x2000, 0, 1, 0,     // Projection - set to 0 for geographic (or use appropriate EPSG)
+    ];
+
+    // Now define the bounding rectangle values
+    let bounding_rectangle = &qm.bounding_rectangle; // Assuming this is your calculated bounding box
+    let ll = bounding_rectangle.lower_left.to_degrees();
+    let ur = bounding_rectangle.upper_right.to_degrees();
+
+    let min_x = ll.lat();
+    let min_y = ll.lon();
+    let max_x = ur.lat();
+    let max_y = ur.lon();
+ 
+    // 0,0 is UL
+    // crs axis order is lat,lon,alt
+    // Define ModelTiepoint
+    let tiepoints: Vec<f64> = vec![
+        // 0.0,    // UL UV x
+        // 0.0,    // UL UV y
+        // 0.0,    // UL UV Z
+        // *min_x, // UL world x
+        // *max_y, // UL world y
+        // 0.0,    // UL world z
+        (raster_size as f64), //LR UV x
+        (raster_size as f64), //LR UV y
+        0.0,  //LR UV z
+        *max_x,  // LR world x
+        *min_y,  // LR world y
+        0.0,     // LR world z
+    ];
+
+    // Define ModelPixelScale (assuming square pixels)
+    let pixel_size_x = (*max_x - *min_x) / raster_size as f64; // Calculate pixel size in X
+    let pixel_size_y = (*max_y - *min_y) / raster_size as f64; // Calculate pixel size in Y
+    let pixel_scale = vec![pixel_size_x, pixel_size_y, 1.0]; // Set Z pixel size to 1 as Z values require no scaling
+
+    // Debugging: Print bounding rectangle coordinates
+    println!("Bounding Rectangle:");
+    println!("Lower Left: ({}, {})", min_x, min_y);
+    println!("Upper Right: ({}, {})", max_x, max_y);
+    println!("Scale: ({:?})", pixel_scale);
+    println!("raster_size: ({:?})", raster_size);
+
+    image.encoder().write_tag(Tag::ModelPixelScaleTag, &pixel_scale[..]).unwrap();
+
+    // Write the ModelTiepointTag
+    image
+        .encoder()
+        .write_tag(Tag::ModelTiepointTag, &tiepoints[..])
+        .unwrap();
+
+    image
+        .encoder()
+        .write_tag(Tag::GeoKeyDirectoryTag, &geo_key_directory[..])
+        .unwrap();
 
     let image_data: Vec<f32> = raster
         .iter()
@@ -90,9 +160,9 @@ pub fn export_to_tiff(
         )
         .into());
     }
+    //image.rows_per_strip(2).unwrap();
 
     // Write the image data as Gray32Float
-    encoder.write_image::<Gray32Float>(raster_size.into(), raster_size.into(), &image_data)?;
-
+    image.write_data(&image_data)?;
     Ok(())
 }
